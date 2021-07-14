e053c10a9b32c23bda8492673082a7121a7cae5a
Aceseck <87353052+Aceseck@users.noreply.github.com>
2021-07-14 11:49:25 +0800

    Update BookDao.java

--- BookBorrow/src/bookborrow/dao/BookDao.java
+++ BookBorrow/src/bookborrow/dao/BookDao.java
@@ -5,4 +5,6 @@ public interface BookDao {
 	public List<Book> getAllBook(); //查询所有书
 	public Book getBook(String sql, Object[]param);//条件查询
 	public int updateBook(String sql, Object[]param);//更新
+	public int addBook(String sql, Object[]param);//添加
+	public int deleteBook(String sql, Object[]param);//删除
 }
